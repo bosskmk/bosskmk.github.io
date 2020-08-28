813d19b98d3631179cd508e997d0efe0845bf5bd
kmk <manekingz@gmail.com>
2020-08-29 05:33:42 +0900

    0.1.4

--- pluto_grid/build/web/flutter_service_worker.js
+++ pluto_grid/build/web/flutter_service_worker.js
@@ -13,7 +13,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "55efb59cfa049b99c8d71cd3ca524fbc",
 "/": "55efb59cfa049b99c8d71cd3ca524fbc",
-"main.dart.js": "0615a1b3f0dee16641bff06b450b08c5",
+"main.dart.js": "f2612b2d06798c341a48abfe0a0bbca4",
 "manifest.json": "9e7b34fd7c291ca2523123ef20497930"
 };
 
