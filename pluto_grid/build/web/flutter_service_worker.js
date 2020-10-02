0000d5ec37e8e564d1f61505f3f142ea9b9dd076
kmk <manekingz@gmail.com>
2020-10-02 20:17:19 +0900

    PlutoGrid 0.1.16

--- pluto_grid/build/web/flutter_service_worker.js
+++ pluto_grid/build/web/flutter_service_worker.js
@@ -29,9 +29,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "4a92db10ba5e16c3e4976d1139fc704e",
-"/": "4a92db10ba5e16c3e4976d1139fc704e",
-"main.dart.js": "042bde5e615d0951efad741630c432e6",
+"index.html": "85e9df0bc1151835dfb8ea533d557ac1",
+"/": "85e9df0bc1151835dfb8ea533d557ac1",
+"main.dart.js": "fe0b6cd6ada28e3884323bbc43a89afc",
 "manifest.json": "9e7b34fd7c291ca2523123ef20497930",
 "version.json": "4b6db237b3514a88107a422469adfb0f"
 };
