ce32b818b17b7ce2d1393c195a3a8ab493ecf74e
kmk <manekingz@gmail.com>
2022-07-18 01:49:45 +0900

    PlutoGrid 5.0.4

--- pluto_grid/build/web/flutter_service_worker.js
+++ pluto_grid/build/web/flutter_service_worker.js
@@ -31,9 +31,9 @@ const RESOURCES = {
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "fee608958b9f120cb4f272b682ab8b60",
-"/": "fee608958b9f120cb4f272b682ab8b60",
-"main.dart.js": "69ce8bff3714614e8c679d8e67922b02",
+"index.html": "3b080207cba3de47677e779ecd5e66f7",
+"/": "3b080207cba3de47677e779ecd5e66f7",
+"main.dart.js": "34e6bd934df17fe36775d09490a50cff",
 "manifest.json": "9e7b34fd7c291ca2523123ef20497930",
 "version.json": "1e98a4f7390d2e9d9ad60c99fb0aff05"
 };
