1b840d81651f3d6b556d41b5f6c3ce60c81800d9
kmk <manekingz@gmail.com>
2022-11-30 11:54:54 +0900

    PlutoMenuBar 3.0.0

--- pluto_menu_bar/build/web/flutter_service_worker.js
+++ pluto_menu_bar/build/web/flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b8d98b1f485dc589ef55fc9bdb08de1b",
-"/": "b8d98b1f485dc589ef55fc9bdb08de1b",
-"main.dart.js": "b761137983b692b664e33c67176041c7",
+"index.html": "72703bea3abe4b2e88feeb8a9044ea35",
+"/": "72703bea3abe4b2e88feeb8a9044ea35",
+"main.dart.js": "b933b59188492b26e4fb5dba7feeaf12",
 "manifest.json": "9a0de1ff26661cd281cb9deb3db33b02",
 "version.json": "ff966ab969ba381b900e61629bfb9789"
 };
